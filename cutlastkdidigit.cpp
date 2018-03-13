f9a10f45c7a8ef8908ddb07230dfc01d1f558d16
brindhasree <36397422+brindhasree@users.noreply.github.com>
2018-03-13 17:43:12 +0530

    Create cutlastkdidigit.cpp

---
+++ cutlastkdidigit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,k,n1;
+	cin>>n>>k;
+	n1=n;
+	for(int i=0;i<k;i++)
+	{
+		n1=n1/10;
+		
+	}
+	cout<<n1;
+	// your code goes here
+	return 0;
+}
